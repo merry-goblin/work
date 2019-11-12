adcc9109ccec518bc098e562ca0e16249cb7f78a
someone <someone@someplace.com>
2019-11-12 14:57:43 +0100

    some tests with joints

--- python/bullet/samples/003-link.py
+++ python/bullet/samples/003-link.py
@@ -61,10 +61,8 @@ def endDisplay():
 
 def initPyBullet():
     p.connect(p.DIRECT)
-
     p.setPhysicsEngineParameter(numSolverIterations=10)
-    p.setTimeStep(1. / 60.)
-    p.setGravity(0, 0, -9.81)
+    p.setTimeStep(1. / 240.)
 
 def main():
     pygame.init()
@@ -93,55 +91,10 @@ def main():
     box2Visualizer.prepare(shaderProgram)
     planeVisualizer.prepare(shaderProgram)
 
-    initPyBullet()
-
     shift     = [0.0, 0.0, 0.0]
     meshScale = [1.0, 1.0, 1.0]
 
-    # Box 1
-    collisionBoxId = p.createCollisionShape(shapeType=p.GEOM_MESH,
-                                              fileName="data/box-T2F_N3F_V3F.obj",
-                                              collisionFramePosition=shift,
-                                              meshScale=meshScale)
-    boxId = p.createMultiBody(baseMass=1,
-                               baseInertialFramePosition=[0, 0, 0],
-                               baseCollisionShapeIndex=collisionBoxId,
-                               basePosition=boxPos,
-                               baseOrientation=boxOrn,
-                               useMaximalCoordinates=True)
-
-    # Box 2
-    linkMasses = [1]
-    linkCollisionShapeIndices = [boxId]
-    linkVisualShapeIndices = [-1]
-    linkPositions = [[0, 0, 0]]
-    linkOrientations = [[0, 0, 0, 1]]
-    linkInertialFramePositions = [[0, 0, 0]]
-    linkInertialFrameOrientations = [[0, 0, 0, 1]]
-    indices = [0]
-    jointTypes = [p.JOINT_REVOLUTE]
-    axis = [[1, 0, 0]]
-
-    collisionBox2Id = p.createCollisionShape(shapeType=p.GEOM_MESH,
-                                              fileName="data/box-T2F_N3F_V3F.obj",
-                                              collisionFramePosition=shift,
-                                              meshScale=meshScale)
-    box2Id = p.createMultiBody(baseMass=1,
-                               baseInertialFramePosition=[0, 0, 0],
-                               baseCollisionShapeIndex=collisionBox2Id,
-                               basePosition=boxPos2,
-                               baseOrientation=boxOrn2,
-                               useMaximalCoordinates=True,
-                               linkMasses=linkMasses,
-                               linkCollisionShapeIndices=linkCollisionShapeIndices,
-                               linkVisualShapeIndices=linkVisualShapeIndices,
-                               linkPositions=linkPositions,
-                               linkOrientations=linkOrientations,
-                               linkInertialFramePositions=linkInertialFramePositions,
-                               linkInertialFrameOrientations=linkInertialFrameOrientations,
-                               linkParentIndices=indices,
-                               linkJointTypes=jointTypes,
-                               linkJointAxis=axis)
+    initPyBullet()
 
     # Plane
     collisionPlaneId = p.createCollisionShape(shapeType=p.GEOM_MESH,
@@ -156,12 +109,119 @@ def main():
                                baseOrientation=planeOrn,
                                useMaximalCoordinates=True)
 
+    # Box 1
+    collisionBoxId = p.createCollisionShape(shapeType=p.GEOM_MESH,
+                                              fileName="data/box-T2F_N3F_V3F.obj",
+                                              collisionFramePosition=shift,
+                                              meshScale=meshScale)
+    collisionBox2Id = p.createCollisionShape(shapeType=p.GEOM_MESH,
+                                              fileName="data/box-T2F_N3F_V3F.obj",
+                                              collisionFramePosition=shift,
+                                              meshScale=meshScale)
+    """
+    linkMasses = []
+    linkCollisionShapeIndices = []
+    linkVisualShapeIndices = []
+    linkPositions = []
+    linkOrientations = []
+    linkInertialFramePositions = []
+    linkInertialFrameOrientations = []
+    indices = []
+    jointTypes = []
+    axis = []
+
+    linkMasses.append(1)
+    linkCollisionShapeIndices.append(collisionBoxId)
+    linkVisualShapeIndices.append(-1)
+    linkPositions.append([0, 1 + 0.01, 0])
+    linkOrientations.append([0, 0, 0, 1])
+    linkInertialFramePositions.append([0, 0, 0])
+    linkInertialFrameOrientations.append([0, 0, 0, 1])
+    indices.append(1)
+    jointTypes.append(p.JOINT_REVOLUTE)
+    axis.append([0, 0, 1])
+    """
+    useMaximalCoordinates = True
+    mass = 1
+    visualShapeId = -1
+    linkMasses = []
+    linkCollisionShapeIndices = []
+    linkVisualShapeIndices = []
+    linkPositions = []
+    linkOrientations = []
+    linkInertialFramePositions = []
+    linkInertialFrameOrientations = []
+    indices = []
+    jointTypes = []
+    axis = []
+
+    for i in range(6):
+      linkMasses.append(1)
+      linkCollisionShapeIndices.append(collisionBox2Id)
+      linkVisualShapeIndices.append(-1)
+      linkPositions.append([0, 2.0 + 0.01, 0])
+      linkOrientations.append([0, 0, 0, 1])
+      linkInertialFramePositions.append([0, 0, 0])
+      linkInertialFrameOrientations.append([0, 0, 0, 1])
+      indices.append(i)
+      jointTypes.append(p.JOINT_REVOLUTE)
+      axis.append([0, 0, 1])
+
+    boxId = p.createMultiBody(mass,
+                              collisionBoxId,
+                              visualShapeId,
+                              boxPos,
+                              boxOrn,
+                              linkMasses=linkMasses,
+                              linkCollisionShapeIndices=linkCollisionShapeIndices,
+                              linkVisualShapeIndices=linkVisualShapeIndices,
+                              linkPositions=linkPositions,
+                              linkOrientations=linkOrientations,
+                              linkInertialFramePositions=linkInertialFramePositions,
+                              linkInertialFrameOrientations=linkInertialFrameOrientations,
+                              linkParentIndices=indices,
+                              linkJointTypes=jointTypes,
+                              linkJointAxis=axis,
+                              useMaximalCoordinates=useMaximalCoordinates)
+
+    """
+    linkMasses = [1]
+    linkCollisionShapeIndices = [collisionBox2Id]
+    linkVisualShapeIndices = [-1]
+    linkPositions = [[0, 2, 0]]
+    linkOrientations = [[0, 0, 0, 1]]
+    linkInertialFramePositions = [[0, 0, 0]]
+    linkInertialFrameOrientations = [[0, 0, 0, 1]]
+    indices = [1]
+    jointTypes = [p.JOINT_REVOLUTE]
+    axis = [[0, 0, 1]]
+    
+
+    boxId = p.createMultiBody(baseMass=1,
+                              baseCollisionShapeIndex=collisionBoxId,
+                              basePosition=boxPos,
+                              baseOrientation=boxOrn,
+                              linkMasses=linkMasses,
+                              linkCollisionShapeIndices=linkCollisionShapeIndices,
+                              linkVisualShapeIndices=linkVisualShapeIndices,
+                              linkPositions=linkPositions,
+                              linkOrientations=linkOrientations,
+                              linkInertialFramePositions=linkInertialFramePositions,
+                              linkInertialFrameOrientations=linkInertialFrameOrientations,
+                              linkParentIndices=indices,
+                              linkJointTypes=jointTypes,
+                              linkJointAxis=axis,
+                              useMaximalCoordinates=True)
+    """
+
+    p.setGravity(0, 0, -9.81)
+    p.setRealTimeSimulation(0)
 
     clock = pygame.time.Clock()
     done  = False
     tick  = 0
     applyForce = False
-    force = (0,0,100)
+    force = (0,0,5000)
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -172,7 +232,6 @@ def main():
         tick += 1
 
         p.stepSimulation()
-
         if (applyForce) :
             applyForce = False
             forcePos = [0.0, 0.0, 0.0]
@@ -184,18 +243,18 @@ def main():
 
         boxPos, boxOrn = p.getBasePositionAndOrientation(boxId)
         boxVisualizer.updatePosAndOrn(boxPos, boxOrn)
-        boxPos2, boxOrn2 = p.getBasePositionAndOrientation(box2Id)
-        box2Visualizer.updatePosAndOrn(boxPos2, boxOrn2)
+        #boxPos2, boxOrn2 = p.getBasePositionAndOrientation(box2Id)
+        #box2Visualizer.updatePosAndOrn(boxPos2, boxOrn2)
         planePos, planeOrn = p.getBasePositionAndOrientation(planeId)
         planeVisualizer.updatePosAndOrn(planePos, planeOrn)
 
         startDisplay()
         boxVisualizer.draw(shaderProgram, tick/60)
-        box2Visualizer.draw(shaderProgram, tick/60)
+        #box2Visualizer.draw(shaderProgram, tick/60)
         planeVisualizer.draw(shaderProgram, tick/60)
         endDisplay()
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(240)
 
 if __name__ == '__main__':
     try:
---
+++ python/bullet/samples/snake2.py
@@ -0,0 +1,67 @@
+import pybullet as p
+import time
+import math
+
+p.connect(p.GUI)
+
+plane = p.createCollisionShape(p.GEOM_PLANE)
+p.createMultiBody(0, plane)
+
+useMaximalCoordinates = True
+sphereRadius = 0.25
+#colBoxId = p.createCollisionShapeArray([p.GEOM_BOX, p.GEOM_SPHERE],radii=[sphereRadius+0.03,sphereRadius+0.03], halfExtents=[[sphereRadius,sphereRadius,sphereRadius],[sphereRadius,sphereRadius,sphereRadius]])
+colBoxId = p.createCollisionShape(p.GEOM_BOX,
+                                  halfExtents=[sphereRadius, sphereRadius, sphereRadius])
+
+mass = 1
+visualShapeId = -1
+
+link_Masses = []
+linkCollisionShapeIndices = []
+linkVisualShapeIndices = []
+linkPositions = []
+linkOrientations = []
+linkInertialFramePositions = []
+linkInertialFrameOrientations = []
+indices = []
+jointTypes = []
+axis = []
+
+for i in range(36):
+  link_Masses.append(1)
+  linkCollisionShapeIndices.append(colBoxId)
+  linkVisualShapeIndices.append(-1)
+  linkPositions.append([0, sphereRadius * 2.0 + 0.01, 0])
+  linkOrientations.append([0, 0, 0, 1])
+  linkInertialFramePositions.append([0, 0, 0])
+  linkInertialFrameOrientations.append([0, 0, 0, 1])
+  indices.append(i)
+  jointTypes.append(p.JOINT_REVOLUTE)
+  axis.append([0, 0, 1])
+
+basePosition = [0, 0, 1]
+baseOrientation = [0, 0, 0, 1]
+sphereUid = p.createMultiBody(mass,
+                              colBoxId,
+                              visualShapeId,
+                              basePosition,
+                              baseOrientation,
+                              linkMasses=link_Masses,
+                              linkCollisionShapeIndices=linkCollisionShapeIndices,
+                              linkVisualShapeIndices=linkVisualShapeIndices,
+                              linkPositions=linkPositions,
+                              linkOrientations=linkOrientations,
+                              linkInertialFramePositions=linkInertialFramePositions,
+                              linkInertialFrameOrientations=linkInertialFrameOrientations,
+                              linkParentIndices=indices,
+                              linkJointTypes=jointTypes,
+                              linkJointAxis=axis,
+                              useMaximalCoordinates=useMaximalCoordinates)
+
+p.setGravity(0, 0, -10)
+p.setRealTimeSimulation(0)
+
+dt = 1. / 240.
+while (1):
+  p.stepSimulation()
+  time.sleep(dt)
